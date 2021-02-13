c3ca5d1aacc809b0e4d94889520a9f212d2cf293
LENOVO 330 <LENOVO 330@DESKTOP-16BSVEN>
2021-02-13 11:41:06 -0600

    1.1

---
+++ src/gui/Main.java
@@ -0,0 +1,30 @@
+/*
+ Nombre: Josue Julian Leon Garcia
+ Matricula: 2022995
+ */
+package gui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author LENOVO 330
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame marco = new JFrame();
+        marco.setSize(500, 500);
+        marco.setVisible(true);
+        marco.setTitle("Ventana :}");
+        
+        marco.setResizable(true);
+        //marco.getContentPane().setBackground(Color.green);
+        marco.getContentPane().setBackground(new Color(28, 26, 198));
+        
+
+
+
+    }
+}
